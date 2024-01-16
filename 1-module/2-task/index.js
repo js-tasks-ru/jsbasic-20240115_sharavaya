15e45eb1c3407cc45ed607c1c2421ded48baf0a8
GeorgeAlexeev <ruzetixli@gmail.com>
2024-01-16 22:26:45 +0300

    3rd task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,15 +10,19 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name !== null && !name.includes(" ") && name.length >= 4) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
