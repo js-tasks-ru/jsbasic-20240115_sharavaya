d2caaecb6cfdf3948553f4a6959421b1db698f58
GeorgeAlexeev <134794357+GeorgeAlexeev@users.noreply.github.com>
2024-03-11 21:01:49 +0300

    Update index.js

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -1,9 +1,5 @@
-
 import createElement from "../../assets/lib/create-element.js";
 import ProductCard from "../../6-module/2-task/index.js";
-
-
-
 
 export default class ProductGrid {
   constructor(products) {
